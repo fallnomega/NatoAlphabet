e8b66db8066feb05c57ec1df55c32f27af74ba2e
fallnomega <fallnomega@gmail.com>
2023-02-15 13:08:43 -0800

    pull phonetic alphabet from csv file

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+#Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+with open("nato_phonetic_alphabet.csv") as file:
+    temp = pandas.read_csv(file)
+    phonetic_alphabet = {row.letter:row.code for (index,row) in temp.iterrows()}
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
+
+
+
+exit()
